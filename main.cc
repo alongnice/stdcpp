f5af414b66e5b0fd0de3f6c06b1758b26039c7b9
ALONG <731497705@qq.com>
2022-06-20 20:39:46 +0800

    cin some question

--- main.cc
+++ main.cc
@@ -7,17 +7,20 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include "complie.h"
-using namespace std;
+//using namespace std;
 int main() {
-    complie a(3, 4),b(1,2);
-    complie c=++a;
-    c.get_all();
-    complie d=b++;
-    d.get_all();
-    cout<<d<<endl;
-    complie e;
-    cin>>e;
-    cout<<e;
-    //  7-6
+    int a;
+    int index=0;
+    while(cin>>a){
+        cout<<a<<endl;
+        index++;
+        if(index==5)
+            break;;
+    }
+    cin.ignore(1024,'\n');  //清空缓存区脏数据
+    char ch;
+    cin>>ch;
+    cout<<"last char  "<<ch;
+    //  7-9
     return 0;
 }
